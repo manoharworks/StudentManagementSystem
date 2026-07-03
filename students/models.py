de10cff1c6a357489b7d7e516b3af5836a2a8bf7
Manohar Jha <myselfmanohar749@gmail.com>
2026-07-03 15:31:14 +0545

    Added the Search functionality and Pagination

--- students/models.py
+++ students/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=15)
-    date_of_birth = models.DateField()
-    gender = models.CharField(max_length=10)
-    address = models.TextField()
+    email = models.EmailField(unique=True, blank=True, null=True)
+    phone = models.CharField(max_length=15,blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=10,blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
     photo = models.ImageField(upload_to="students/", blank=True, null=True)
 
     def __str__(self):
--- students/views.py
+++ students/views.py
@@ -2,82 +2,107 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
 from .forms import StudentForm
 from django.contrib import messages
-    
-    
+from django.core.paginator import Paginator
+
+
+
 # Create your views here.
 def student_list(request):
+    
+    query = request.GET.get("q")
+
     students = Student.objects.all()
     
-    context = {
-        "students": students
-    }
+
+    if query:
+
+        students = students.filter(name__icontains=query)
+
+    paginator = Paginator(students, 10)
+
+    page_number = request.GET.get("page")
+
+    page_obj = paginator.get_page(page_number)
     
+    context = {
+        "page_obj": page_obj
+    }
+
     return render(request, "students/student_list.html", context)
+
 
 def student_create(request):
     if request.method == "POST":
-        form = StudentForm(request.POST)
-        
+        form = StudentForm(request.POST, request.FILES)
+
         if form.is_valid():
             form.save()
-            
-            return redirect("students:student_list")    
+
+            messages.success(
+                request,
+                "Student created successfully."
+            )
+
+            return redirect("students:student_list")
 
     else:
         form = StudentForm()
-        
+
     context = {
-        "form" : form,
-    }    
-    
+        "form": form,
+    }
+
     return render(request, "students/student_form.html", context)
 
+
 def student_detail(request, pk):
-    
+
     student = get_object_or_404(Student, pk=pk)
-    
+
     context = {
-        "student" : student
+        "student": student
     }
-    
+
     return render(request, "students/student_detail.html", context)
 
 
 def student_update(request, pk):
-    
+
     student = get_object_or_404(Student, pk=pk)
-    
+
     if request.method == "POST":
-        form = StudentForm(request.POST, request.FILES, instance = student)
-        
+        form = StudentForm(request.POST, request.FILES, instance=student)
+
         if form.is_valid():
             form.save()
-            
+
+            messages.success(request, "Student detail updated successfully.")
+
             return redirect("students:student_detail",
-                            pk = student.pk)
-            
-            
+                            pk=student.pk)
+
     else:
-        form = StudentForm(instance = student)
-        
+        form = StudentForm(instance=student)
+
     context = {
-        "form" : form,
+        "form": form,
         "student": student,
-    }    
-                    
+    }
+
     return render(request, "students/student_form.html", context)
-                
-                
+
+
 def student_delete(request, pk):
-    
+
     student = get_object_or_404(Student, pk=pk)
-    
+
     if request.method == "POST":
-        
+
         student.delete()
-        
-        return redirect ("students:student_list")
-    
+
+        messages.success(request, "Student deleted successfully.")
+        return redirect("students:student_list")
+
     context = {
         "student": student
     }
