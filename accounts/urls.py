e7189f231979ee0deaefc4efd1f8a895afa9958c
Manohar Jha <myselfmanohar749@gmail.com>
2026-07-05 21:43:21 +0545

    Added the function of password changing

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from .views import *
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("signup/", signup, name = "signup" ),
     path("profile/", profile, name = "profile"),
+    path("password-change/", auth_views.PasswordChangeView.as_view(template_name = "accounts/password_change.html", success_url = reverse_lazy("accounts:password_change_done")), name = "password_change"),
+    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(template_name = "accounts/password_change_done.html"), name = "password_change_done"),
+    
 ]
-
